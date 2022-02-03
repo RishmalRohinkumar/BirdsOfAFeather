b1bfa28fa3bcab891669522b831531ce03851d55
RishmalRohinkumar475 <57371405+RishmalRohinkumar@users.noreply.github.com>
2022-02-03 15:24:30 -0800

    Update MainActivity and JUnit files to be Java

--- app/src/main/java/com/example/birdsofafeatherteam14/MainActivity.java
+++ app/src/main/java/com/example/birdsofafeatherteam14/MainActivity.java
@@ -1,17 +1,14 @@
-package com.example.birdsofafeatherteam14
+package com.example.birdsofafeatherteam14;
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-    // spawn the name activity, then picture activity, then courses activity
-    // the main activity screen itself is the search page with the "search" button
-
 }
