dce15783e153f5c0edd7bbfd5350d3dbdd8a0a63
jinuk6789 <jinuk6789@gmail.com>
2022-02-06 19:53:39 -0800

    The photo retrieval is working

--- app/src/main/java/com/example/birdsofafeatherteam14/MainActivity.java
+++ app/src/main/java/com/example/birdsofafeatherteam14/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.birdsofafeatherteam14;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String profile_pic_url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(this, ProfilePictureActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent,1);
 
+        //Log.i("url prints here", profile_pic_url);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                this.profile_pic_url = data.getStringExtra("url");
+            }
+
+        }
     }
 }
