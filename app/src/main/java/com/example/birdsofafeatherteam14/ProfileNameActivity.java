2823f4228d51b0737ae5cbb63cc90a6f602b6fc4
Tyler Lentz <tlentz@ucsd.edu>
2022-02-06 19:01:55 -0800

    In theory, google log in should work

--- app/src/main/java/com/example/birdsofafeatherteam14/ProfileNameActivity.java
+++ app/src/main/java/com/example/birdsofafeatherteam14/ProfileNameActivity.java
@@ -4,13 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class ProfileNameActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_name);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account == null) {
+            System.out.println("1");
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        } else {
+            System.out.println("2");
+            setTextToFirstName(account);
+        }
+    }
+
+    private void setTextToFirstName(GoogleSignInAccount account) {
+        System.out.println("3");
+        TextView textView = (TextView) findViewById(R.id.editTextTextPersonName);
+        textView.setText(account.getDisplayName());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        System.out.println("4");
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            setTextToFirstName(completedTask.getResult(ApiException.class));
+        } catch (ApiException e) {
+            // the text input remains empty
+        }
     }
 }
