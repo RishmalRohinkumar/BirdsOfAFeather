5657dc09571b114a2ffdf0de82ad28a3a0cea263
Tyler Lentz <tlentz@ucsd.edu>
2022-02-07 15:28:33 -0800

    Fixed pulling full name instead of just first name

--- app/src/main/java/com/example/birdsofafeatherteam14/ProfileNameActivity.java
+++ app/src/main/java/com/example/birdsofafeatherteam14/ProfileNameActivity.java
@@ -35,26 +35,28 @@ public class ProfileNameActivity extends AppCompatActivity {
 
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         if (account == null) {
-            System.out.println("1");
             Intent signInIntent = mGoogleSignInClient.getSignInIntent();
             startActivityForResult(signInIntent, RC_SIGN_IN);
         } else {
-            System.out.println("2");
             setTextToFirstName(account);
         }
     }
 
     private void setTextToFirstName(GoogleSignInAccount account) {
-        System.out.println("3");
         TextView textView = (TextView) findViewById(R.id.editTextTextPersonName);
-        textView.setText(account.getDisplayName());
+        String fullName = account.getDisplayName();
+        String[] splitName = fullName.split(" ");
+        String firstName = "";
+        if (splitName.length >= 1) {
+            firstName = splitName[0];
+        }
+        textView.setText(firstName);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        System.out.println("4");
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             // The Task returned from this call is always completed, no need to attach
