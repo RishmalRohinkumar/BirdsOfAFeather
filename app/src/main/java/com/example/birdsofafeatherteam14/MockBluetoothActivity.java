d14e57c3bc42cd5503e859ee5901a0fa647e2720
Tyler Lentz <tlentz@ucsd.edu>
2022-02-12 16:06:21 -0800

    Added mock bluetooth activity

---
+++ app/src/main/java/com/example/birdsofafeatherteam14/MockBluetoothActivity.java
@@ -0,0 +1,14 @@
+package com.example.birdsofafeatherteam14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MockBluetoothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mock_bluetooth);
+    }
+}
