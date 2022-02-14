6eb8bcd3b6927fb2c2f212bd6b0ed1088032bea4
Tyler Lentz <tlentz@ucsd.edu>
2022-02-13 21:49:04 -0800

    Added image and shared course num to the recycler view

--- app/src/main/java/com/example/birdsofafeatherteam14/StudentViewAdapter.java
+++ app/src/main/java/com/example/birdsofafeatherteam14/StudentViewAdapter.java
@@ -5,21 +5,25 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.birdsofafeatherteam14.model.db.Student;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class StudentViewAdapter extends RecyclerView.Adapter<StudentViewAdapter.ViewHolder> {
     private final List<? extends Student> students;
+    private final List<Integer> sharedCourses;
 
-    public StudentViewAdapter(List<? extends Student> students) {
+    public StudentViewAdapter(List<? extends Student> students, List<Integer> sharedCourses) {
         super();
         this.students = students;
+        this.sharedCourses = sharedCourses;
     }
 
     @NonNull
@@ -34,7 +38,7 @@ public class StudentViewAdapter extends RecyclerView.Adapter<StudentViewAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull StudentViewAdapter.ViewHolder holder, int position) {
-        holder.setStudent(students.get(position));
+        holder.setStudent(students.get(position), sharedCourses.get(position));
     }
 
     @Override
@@ -46,17 +50,20 @@ public class StudentViewAdapter extends RecyclerView.Adapter<StudentViewAdapter.
             extends RecyclerView.ViewHolder
             implements View.OnClickListener {
         private final TextView studentNameView;
+        private final ImageView studentImageView;
         private Student student;
 
         ViewHolder(View itemView) {
             super(itemView);
             this.studentNameView = itemView.findViewById(R.id.student_row_name);
+            this.studentImageView = itemView.findViewById(R.id.row_view_picture);
             itemView.setOnClickListener(this);
         }
 
-        public void setStudent(Student student) {
+        public void setStudent(Student student, Integer sharedCourses) {
             this.student = student;
-            this.studentNameView.setText(student.getName());
+            this.studentNameView.setText(student.getName() + "(" + sharedCourses.toString() + ")");
+            Picasso.get().load(student.getPhoto()).into(this.studentImageView);
         }
 
         @Override
