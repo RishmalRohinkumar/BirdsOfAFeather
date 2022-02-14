0cdf8b45c1445d54ecbe1bfd277f355341dbdef2
Tyler Lentz <tlentz@ucsd.edu>
2022-02-13 19:03:47 -0800

    Added tests for studentcoursecomparator

---
+++ app/src/test/java/com/example/birdsofafeatherteam14/StudentCourseComparatorTests.java
@@ -0,0 +1,53 @@
+package com.example.birdsofafeatherteam14;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.example.birdsofafeatherteam14.model.db.Course;
+import com.example.birdsofafeatherteam14.model.db.StudentDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentCourseComparatorTests {
+    List<Course> s1_courses;
+    List<Course> s2_courses;
+
+    @Before
+    public void setUp() {
+        s1_courses = new ArrayList<Course>();
+        s2_courses = new ArrayList<Course>();
+    }
+
+    @Test
+    public void testEmptyLists() {
+        StudentCourseComparator scc = new StudentCourseComparator(s1_courses, s2_courses);
+        assertEquals(true, scc.compare().isEmpty());
+    }
+
+    @Test
+    public void testNoOverlapLists() {
+        s1_courses.add(new Course(0, 0, 2020, 110, "CSE", "FA"));
+        s1_courses.add(new Course(0, 0, 2022, 120, "CSE", "SP"));
+        s2_courses.add(new Course(0, 0, 2022, 120, "PHYS", "SP"));
+        StudentCourseComparator scc = new StudentCourseComparator(s1_courses, s2_courses);
+        assertEquals(true, scc.compare().isEmpty());
+    }
+
+    @Test
+    public void testOverlapLists() {
+        s1_courses.add(new Course(0, 0, 2020, 110, "CSE", "FA"));
+        s1_courses.add(new Course(0, 0, 2022, 120, "CSE", "SP"));
+        s2_courses.add(new Course(0, 0, 2022, 120, "CSE", "SP"));
+        StudentCourseComparator scc = new StudentCourseComparator(s1_courses, s2_courses);
+        List<Course> overlap = scc.compare();
+        assertEquals(1, overlap.size());
+        Course overlapCourse = overlap.get(0);
+        assertEquals(120, overlapCourse.courseNumber);
+        assertEquals(2022, overlapCourse.courseYear);
+        assertEquals("CSE", overlapCourse.courseName);
+        assertEquals("SP", overlapCourse.courseQuarter);
+    }
+}
