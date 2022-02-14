8c1d7c6bc06f94c1f3229fece1b7488e37d80290
Tyler Lentz <tlentz@ucsd.edu>
2022-02-13 20:34:40 -0800

    Added new test

--- app/src/androidTest/java/com/example/birdsofafeatherteam14/testAddStudentAndCoursesToDb.java
+++ app/src/androidTest/java/com/example/birdsofafeatherteam14/testAddStudentAndCoursesToDb.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 public class testAddStudentAndCoursesToDb {
@@ -32,6 +33,7 @@ public class testAddStudentAndCoursesToDb {
 
     @After
     public void closeDb() throws IOException {
+        db.clearAllTables();
         db.close();
     }
 
@@ -39,16 +41,35 @@ public class testAddStudentAndCoursesToDb {
     public void addStudentsAndCoursesAndCheckSize() throws Exception {
         Student student0 = new Student(db.studentDAO().count()+1, "Tyler", "");
         db.studentDAO().insert(student0);
-        Course course0 = new Course(student0.studentId, db.coursesDAO().count() + 1, 2022, 120, "CSE", "SP");
+        Course course0 = new Course(db.coursesDAO().count() + 1, student0.studentId, 2022, 120, "CSE", "SP");
         db.coursesDAO().insert(course0);
         Student student1 = new Student(db.studentDAO().count()+1, "Tyler1", "");
         db.studentDAO().insert(student1);
-        Course course1 = new Course(student1.studentId, db.coursesDAO().count() + 1, 2022, 139, "CSE", "SP");
+        Course course1 = new Course(db.coursesDAO().count() + 1, student1.studentId, 2022, 139, "CSE", "SP");
         db.coursesDAO().insert(course1);
 
         assertEquals(2, db.studentDAO().count());
         assertEquals(2, db.coursesDAO().count());
+    }
 
+    @Test
+    public void testGetCoursesByStudent() throws Exception {
+        Student student0 = new Student(db.studentDAO().count()+1, "Tyler", "");
+        db.studentDAO().insert(student0);
+        Course course0 = new Course(db.coursesDAO().count() + 1, student0.studentId, 2022, 120, "CSE", "SP");
+        db.coursesDAO().insert(course0);
+        Course course1 = new Course(db.coursesDAO().count() + 1, student0.studentId, 2023, 125, "PHYS", "FA");
+        db.coursesDAO().insert(course1);
+        Student student1 = new Student(db.studentDAO().count()+1, "Tyler1", "");
+        db.studentDAO().insert(student1);
+        Course course2 = new Course(db.coursesDAO().count() + 1, student1.studentId, 2022, 139, "CSE", "SP");
+        db.coursesDAO().insert(course2);
 
+        List<Course> coursesFromDbTyler = db.coursesDAO().getForStudent(student0.studentId);
+        assertEquals(2, coursesFromDbTyler.size());
+        Course courseFromDb0 = coursesFromDbTyler.get(0);
+        Course courseFromDb1 = coursesFromDbTyler.get(1);
+        assertEquals(true, courseFromDb0.equals(course0));
+        assertEquals(true, courseFromDb1.equals(course1));
     }
 }
