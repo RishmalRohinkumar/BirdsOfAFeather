4e2d83b3413c290e76fdfe9456e8f5fcc50a7410
Tyler Lentz <tlentz@ucsd.edu>
2022-03-10 16:50:51 -0800

    Increased size of wave button so all of msg would fit in it

---
+++ app/src/test/java/com/example/birdsofafeatherteam14/WaveMessageTranslatorTests.java
@@ -0,0 +1,11 @@
+package com.example.birdsofafeatherteam14;
+
+import org.junit.Test;
+
+public class WaveMessageTranslatorTests {
+    @Test
+    public void testTODO() {
+        // https://canvas.ucsd.edu/courses/32700/assignments/451849
+        // make student with same info as the
+    }
+}
